6b4381f717c9fbbcc94f6ba998a3c420a9d0a55a
Ahad-Dar <79365354+Ahad-Dar@users.noreply.github.com>
2022-11-29 01:36:37 -0800

    Update server.js

--- server.js
+++ server.js
@@ -3,15 +3,7 @@ var zetta = require('zetta');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 var PORT = process.env.PORT || 3000;
 var pidlock = require('pidlock');
-pidlock.guard('/tmp', 'supercalifragilisticexpialidocious', function(error, data, cleanup) {
-  if (!error) {
-    // Should run.
-    run();
-    cleanup();
-  } else {
-    // Another instance is already running.
-  }
-});
+
 
 zetta()
   .name('cloud')
