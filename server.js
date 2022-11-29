3313ada7c1b501ed5366fa0affde1ceaeaa990e7
Ahad-Dar <79365354+Ahad-Dar@users.noreply.github.com>
2022-11-29 10:32:08 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
-process.EventEmitter = require('events').EventEmitter;
+process.EventEmitter=require('events').EventEmitter
 var zetta = require('zetta');
+var StateMachineDevice = require('./device.js');
+var port = process.env.PORT || 3000;
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-var PORT = process.env.PORT || 3000;
-
-
-zetta()
-  .name('cloud')
-  .expose('*')
-  .listen(PORT, function(err) {
-    if(err) {
-      console.error(err);
-      process.exit(1);
-    }
-    console.log('running on http://localhost:', PORT)
+  zetta()
+    .name('State Machine Server')
+    .use(StateMachineDevice)
+    .listen(port, function(){
+       console.log('Zetta is running at http://127.0.0.1:3000');
   });
